d0cd1f603ceae2271b96de7288e93d42532d53ee
angelfraga <angel.fraga.parodi@leanix.net>
2020-05-11 11:14:50 +0200

    add resizing example with canvas element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,13 @@ export class AppComponent  implements AfterViewInit, OnDestroy {
     this.componentDestroyed$.next();
     this.componentDestroyed$.complete();
   }
+
+  /**
+   * example of resizing another element based on resized event
+   * btw , we could call render method afterward
+   */
+  resizeElement(coords: ClientRect | DOMRect, element: HTMLCanvasElement) {
+    // element.width = coords.width;
+    // element.height = coords.height;
+  }
 }
